e2f7a1b8d096dc6f88c7745828567d149300031a
jticer4 <jticer@cnm.edu>
2018-06-29 17:50:38 -0600

    added my images and declared them as new instances of the image class

--- FlappyBurqueno.js
+++ FlappyBurqueno.js
@@ -2,3 +2,27 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+//load images as instances of the image class
+
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src= "images/bird.png";
+bg.src = "images/bg.png";
+fg.src = "images/fg.png";
+pipeNorth.src = "images/pipeNorth.png";
+pipeSouth.src = "images/pipeSouth.png";
+
+// draw images
+
+function draw() {
+
+	ctx.drawImage(bg,0,0);
+
+
+}
+
+draw();
