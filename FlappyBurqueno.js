2eb57460f4a6f77f2400b43ee61000adea4e2a7f
jticer4 <jticer@cnm.edu>
2018-06-30 14:28:43 -0600

    added in my north and south pipes, along with two variables to align my south pipe on the y axis at a constant distance from the north pipe. I also added in the foreground image and aligned it at its as far down on the y axis as possible, without loosing any of the image itself.

--- FlappyBurqueno.js
+++ FlappyBurqueno.js
@@ -2,7 +2,6 @@
 let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
 
-
 //load images
 
 let bird = new Image();
@@ -17,11 +16,23 @@ fg.src = "images/fg.png";
 pipeNorth.src = "images/pipeNorth.png";
 pipeSouth.src = "images/pipeSouth.png";
 
+//declare variables
+//set the gap to 75 pixels which will be applied to the y axis
+let gap = 80;
+//set the constant variable to the height of the north pipe plus the gap
+let constant = pipeNorth.height + gap;
 // draw images
 
 window.onload = function draw() {
 
+	//draw the background
 	ctx.drawImage(bg, 0, 0);
 
+	//draw the north and south pipes
+	ctx.drawImage(pipeNorth, 100, 0);
+	ctx.drawImage(pipeSouth, 100, constant);
+
+	//draw the foreground
+	ctx.drawImage(fg, 0, cvs.height - fg.height);
 
 }
